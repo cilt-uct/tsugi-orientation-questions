87b5d8aeff762d5ac23728620ee8cbdeb8fd5b2c
TurRil <lightscape2001@gmail.com>
2020-04-16 12:32:23 +0200

    Loading of saved value, display and switching of labels and messages, saving of value.

--- database.php
+++ database.php
@@ -11,23 +11,12 @@ array( "{$CFG->dbprefix}Orientation_Questions",
 "create table {$CFG->dbprefix}Orientation_Questions (
     `link_id` int(11) NOT NULL,
     `user_id` int(11) NOT NULL,
+    `EID` varchar(45) NOT NULL,
     `answer` tinyint(1) NOT NULL,
     `ipaddr` varchar(64) DEFAULT NULL,
     `created` datetime DEFAULT CURRENT_TIMESTAMP,
     `updated` datetime DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT `{$CFG->dbprefix}orientation_ibfk_1`
-        FOREIGN KEY (`link_id`)
-        REFERENCES `{$CFG->dbprefix}lti_link` (`link_id`)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-
-    CONSTRAINT `{$CFG->dbprefix}orientation_ibfk_2`
-        FOREIGN KEY (`user_id`)
-        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-
-    UNIQUE(link_id, user_id),
+    PRIMARY KEY (`EID`),
     KEY `IDX_ANSWER` (`answer`)
-
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8")
 );
--- src/AppBundle/Home.php
+++ src/AppBundle/Home.php
@@ -12,106 +12,52 @@ use \Tsugi\Util\Net;
 class Home {
 
     public function get(Request $request, Application $app)
-	    {
+    {
         global $CFG, $PDOX;
+        $p = $CFG->dbprefix;
 
-	$context = array();
-
-	    $providers = $app['tsugi']->context->launch->ltiRawParameter('lis_course_section_sourcedid','none');
-        $context_id = $app['tsugi']->context->launch->ltiRawParameter('context_id','none');
-
-    	$context['style'] = $CFG->getCurrentFileUrl('static/user.css');
-	    $context['submit'] = addSession($CFG->getCurrentFileUrl('index.php'));
-        $context['providers'] = array();
-        $context['provider'] = 'none';
+        $EID = $app['tsugi']->context->launch->ltiRawParameter('lis_person_sourcedid', $app['tsugi']->user->id);
         
-        if ($providers != $context_id) {
-            // So we might have some providers to show
-                $list = explode('+', $providers);
-                
-            if (count($list) == 1) {
-                $context['provider'] = $list[0];
-            } else {
-                $context['providers'] = $list;
-            }
+        $context = array();
+    	$context['style'] = $CFG->getCurrentFileUrl('static/user.css');
+	    $context['submit'] = addSession('/tsugi/mod/tsugi-orientation-questions/index.php'); //$CFG->getCurrentFileUrl('index.php'));
+        
+        $rows = $PDOX->allRowsDie("SELECT answer FROM {$p}Orientation_Questions
+                    WHERE EID = :EID and user_id = :user_id",
+                    array(':user_id' => $app['tsugi']->user->id, ':EID' => $EID)
+                );
+        if (count($rows) > 0) {
+            $context['selected'] = $rows[0];
+        } else {
+            $context['selected'] = array('answer' => -1);
         }
         
         $context['path'] = $CFG->staticroot;
-        $context['course_title'] = $app['tsugi']->context->title;
         $context['email'] = $app['tsugi']->user->email;
         $context['user'] = $app['tsugi']->user->displayname;
-        $context['username'] = $app['tsugi']->context->launch->ltiRawParameter('lis_person_sourcedid', $app['tsugi']->user->id);         
+        $context['username'] = $EID;
 
         return $app['twig']->render('Home.twig', $context);
     }
 
     public function post(Request $request, Application $app) {
         global $CFG, $PDOX;
+        $p = $CFG->dbprefix;
 
-        $result = array_merge(array( 
-            'ext_sakai_server' => $app['tsugi']->context->launch->ltiRawParameter('ext_sakai_server','none')
-            ,'ext_sakai_serverid' => $app['tsugi']->context->launch->ltiRawParameter('ext_sakai_serverid','none') 
-            ,'instructor' => $app['tsugi']->user->instructor
-            ,'siteid' => $app['tsugi']->context->launch->ltiRawParameter('context_id','none')
-            ,'ownerEid' => $app['tsugi']->context->launch->ltiRawParameter('lis_person_sourcedid','none') 
-            ,'ownerEmail' => $app['tsugi']->user->email
-            ,'organizer' => $app['tsugi']->user->displayname
-            ,'language' => 'eng'
-            ,'title' => $app['tsugi']->context->title
-            ,'description' => $app['tsugi']->context->title
-            ,'publisher' => 'University of Cape Town'
-            ,'done' => 0
-            ,'msg'  => 'Application failure.'
-        ), $_POST);
-
-        if ($result['course'] == 'none') {
-            $result['course'] = '';
-        }
-
-        $out = array(
-            'done' => 0
-            ,'msg'  => 'Application failure.'
+        $EID = $app['tsugi']->context->launch->ltiRawParameter('lis_person_sourcedid', $app['tsugi']->user->id);
+        $PDOX->queryDie("INSERT INTO {$p}Orientation_Questions
+            (link_id, user_id, ipaddr, EID, answer, updated)
+            VALUES ( :LI, :UI, :IP, :EID, :answer, NOW() )
+            ON DUPLICATE KEY UPDATE updated = NOW(), answer = :answer",
+            array(
+                ':LI' => $app['tsugi']->link->id,
+                ':UI' => $app['tsugi']->user->id,
+                ':IP' => Net::getIP(),
+                'EID' => $app['tsugi']->context->launch->ltiRawParameter('lis_person_sourcedid','none'),
+                ':answer' => $_POST['val']
+            )
         );
-
-        if ($app['tsugi']->user->instructor) {
-
-            $cmd = NULL;
-            $filename = realpath('tmp') ."/". $result['siteid'] .".json";
-            $fp = fopen($filename, 'w');
-            fwrite($fp, json_encode($result));
-            fclose($fp);
-
-            switch ($result['type']) {
-               case "create":
-                    $cmd = $app['script-add'] .' '. $filename;
-                    break;
-                case "remove":
-                    $cmd = $app['script-remove'] .' '. $result['ext_sakai_server'] .' '. $result['siteid'];
-                default: break;
-            }
-
-            if (!is_null($cmd)) {
-                $result['cmd'] = $cmd; 
-                $result['raw'] = shell_exec($cmd);
-                $result['out'] = json_decode($result['raw']);
-
-                if (json_last_error() === JSON_ERROR_NONE) { 
-                    $out['msg'] = $result['out']->out;
-                    $out['done'] = $result['out']->success;
-                } else {
-                    $out['done'] = 0;
-                    $out['msg'] = json_last_error_msg();
-                } 
-            }
-
-            $fp = fopen($filename, 'w');
-            fwrite($fp, json_encode($result));
-            fclose($fp);
-        } else {
-            $out['done'] = 0;
-            $out['msg']  = 'Must be an instructor to complete this operation.';
-        }
-
-        return json_encode($out);
+        // $app->tsugiFlashSuccess(__('Attendance Recorded...'));
+        return json_encode(['done' => 1, 'answer' => $_POST['val']]);
     }
 }
